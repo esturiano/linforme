b932e3fda0a05d734e3bc480d48493f52ef4cd08
esturiano <esturiano@gmail.com>
2021-12-27 07:07:08 +0300

    Update archforme.sh

--- archforme.sh
+++ archforme.sh
@@ -14,16 +14,16 @@ echo -en '\033[1mPackage\033[33;1m (yay)\033[0m\033[1m. \033[36;1mInstall? (y/n)
 while true; do
     read yn
     case $yn in
-        [Yy]* ) 
-            sudo pacman -S reflector curl go unzip jq --noconfirm
+        [Yy]* )
+            sudo pacman -S git reflector curl go nano unzip --noconfirm
             sudo reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
             git clone https://aur.archlinux.org/yay.git
             cd yay
-            makepkg -si
+            makepkg -sir
             cd ..
             rm -fR yay
             echo -en '\033[32;1mDone.\033[0m'
-            
+
             break;;
         [Nn]* ) echo -en '\033[31;1m...skipped.\033[0m\n'; break;;
         * ) echo -en '\033[1mPlease, type \033[33;1;5m"y"\033[0m or \033[33;1;5m"n"\033[0m.';;
@@ -31,34 +31,15 @@ while true; do
 done
 echo ''
 
-echo -en '\033[1mPackages KDE-plasma\033[33;1m (plasma)\033[0m\033[1m. \033[36;1mInstall? (y/n) \033[0m'
+echo -en '\033[1mInstalling yandex-browser-beta\033[33;1m (+jq!)\033[0m\033[1m. \033[36;1mDo it? (y/n) \033[0m'
 while true; do
     read yn
     case $yn in
-        [Yy]* ) 
-            yay -S xnconvert kde-servicemenus-rootactions yandex-disk yandex-disk-indicator yandex-browser-beta --noconfirm
-            mkdir ~/.esturiano@yandex
-            echo -en '\033[32;1mDone.\033[0m'
-            
-            break;;
-        [Nn]* ) echo -en '\033[31;1m...skipped.\033[0m\n'; break;;
-        * ) echo -en '\033[1mPlease, type \033[33;1;5m"y"\033[0m or \033[33;1;5m"n"\033[0m.';;
-    esac
-done
-echo ''
+        [Yy]* )
 
-echo -en '\033[1mInstalling packages for works\033[33;1m (browsers, office, tools etc.)\033[0m\033[1m. \033[36;1mDo it? (y/n) \033[0m'
-while true; do
-    read yn
-    case $yn in
-        [Yy]* ) 
-        
-            
-            yay -S gparted vlc gimp libreoffice-fresh libreoffice-fresh-ru --noconfirm
-            # Подключение репозитория flathub
-            # sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            yay -S --noconfirm jq yandex-browser-beta
             echo -en '\033[32;1mDone.\033[0m\n'
-            
+
             break;;
         [Nn]* ) echo -en '\033[31;1m...skipped.\033[0m\n'; break;;
         * ) echo -en '\033[1mPlease, type \033[33;1;5m"y"\033[0m or \033[33;1;5m"n"\033[0m.';;
@@ -66,33 +47,69 @@ while true; do
 done
 echo ''
 
+echo -en '\033[1mInstalling yandex-disk\033[33;1m (+GUI indicator)\033[0m\033[1m. \033[36;1mDo it? (y/n) \033[0m'
+while true; do
+    read yn
+    case $yn in
+        [Yy]* )
 
+            yay -S --noconfirm yandex-disk yandex-disk-indicator
+            mkdir ~/.esturiano@yandex
+            echo -en '\033[32;1mDone.\033[0m\n'
+
+            break;;
+        [Nn]* ) echo -en '\033[31;1m...skipped.\033[0m\n'; break;;
+        * ) echo -en '\033[1mPlease, type \033[33;1;5m"y"\033[0m or \033[33;1;5m"n"\033[0m.';;
+    esac
+done
+echo ''
+
+echo -en '\033[1mInstalling daily packages\033[33;1m (utils, office, tools etc.)\033[0m\033[1m. \033[36;1mDo it? (y/n) \033[0m'
+while true; do
+    read yn
+    case $yn in
+        [Yy]* )
+
+
+            yay -S --noconfirm gnome-disk-utility okular imagewriter gimp kdenlive obs-studio blender soundkonverter kde-servicemenus-rootactions libreoffice-fresh libreoffice-fresh-ru
+            echo -en '\033[32;1mDone.\033[0m\n'
+
+            break;;
+        [Nn]* ) echo -en '\033[31;1m...skipped.\033[0m\n'; break;;
+        * ) echo -en '\033[1mPlease, type \033[33;1;5m"y"\033[0m or \033[33;1;5m"n"\033[0m.';;
+    esac
+done
+echo ''
+
+# https://drive.google.com/file/d/1xGEcxrK5V0l4LssQhTEXUwYyTsf4S6pu/view?usp=sharing
 echo -en '\033[1mSystem notification library\033[33;1m (my favorite sounds)\033[0m\033[1m. \033[36;1mInstall? (y/n) \033[0m'
 while true; do
     read yn
     case $yn in
-        [Yy]* ) 
-            fileid="1dxSykLylPIfGwHeOtuoaY9gYXPmo9zXi"
-            filename="mysounds.zip"
+        [Yy]* )
+            fileid="1xGEcxrK5V0l4LssQhTEXUwYyTsf4S6pu"
+            filename="msounds.zip"
             curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
             curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-            unzip mysounds.zip
-            rm mysounds.zip
-            sudo mv mysounds /usr/share/sounds
+            unzip msounds.zip
+            rm msounds.zip
+            sudo mv -r msounds /usr/share/sounds
             echo -en '\033[32;1mDone.\033[0m\n'
-            
+
             break;;
         [Nn]* ) echo -en '\033[31;1m...skipped.\033[0m\n'; break;;
         * ) echo -en '\033[1mPlease, type \033[33;1;5m"y"\033[0m or \033[33;1;5m"n"\033[0m.';;
     esac
 done
 echo ''
+
+
 
 echo -en '\033[1mPackage\033[33;1m "zsh"\033[0m\033[1m. \033[36;1mInstall? (y/n) \033[0m'
 while true; do
     read yn
     case $yn in
-        [Yy]* ) 
+        [Yy]* )
             yay -S zsh --noconfirm
             curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh ;
             chsh -s /usr/bin/zsh;
@@ -101,10 +118,9 @@ while true; do
             mv zsh-syntax-highlighting .zsh-syntax-highlighting
             echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
             git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-            echo -en '\033[33mAdd plugin to the file .zshrc (string plugins =(... zsh-autosuggestions).\033[0m\n'
-            cd
-            zsh
-            nano +71 .zshrc
+            # echo -en '\033[33mAdd plugin to the file .zshrc (string plugins =(... zsh-autosuggestions).\033[0m\n'
+            # cd
+            # nano +71 .zshrc
             echo -en '\033[32;1mDone.\033[0m\n'
             break;;
         [Nn]* ) echo -en '\033[31;1m...skipped.\033[0m\n'; break;;
@@ -119,7 +135,7 @@ echo -en '\033[1mPackage\033[33;1m "master-pdf-editor (free)"\033[0m\033[1m. \03
 while true; do
     read yn
     case $yn in
-        [Yy]* ) 
+        [Yy]* )
             yay -S masterpdfeditor-free --noconfirm
             echo -en '\033[32;1mVersion 4.3.89 installed.\033[0m\n'
             break;;
@@ -134,7 +150,7 @@ echo -en '\033[1mPackage\033[33;1m "glabels-qt (beta)"\033[0m\033[1m. \033[36;1m
 while true; do
     read yn
     case $yn in
-        [Yy]* ) 
+        [Yy]* )
             yay -S glabels-qt-git --noconfirm
             echo -en '\033[32;1mDone.\033[0m\n'
             break;;
@@ -145,12 +161,12 @@ done
 echo ''
 
 
-echo -en '\033[1mDrivers\033[33;1m "nvidia-340xx"\033[0m\033[1m. \033[36;1mInstall? (y/n) \033[0m'
+echo -en '\033[1mDrivers\033[33;1m "nvidia-340xx-lts-dkms"\033[0m\033[1m. \033[36;1mInstall? (y/n) \033[0m'
 while true; do
     read yn
     case $yn in
-        [Yy]* ) 
-            yay -Sy nvidia-340xx-dkms nvidia-340xx-utils lib32-nvidia-340xx-utils opencl-nvidia-340xx lib32-opencl-nvidia-340xx --noconfirm
+        [Yy]* )
+            yay -Sy nvidia-340xx-lts-dkms nvidia-340xx-utils lib32-opencl-nvidia-340xx opencl-nvidia-340xx --noconfirm
             sudo nvidia-xconfig --no-logo
             echo -en '\033[32;1mDone.\033[0m\n'
             break;;
@@ -164,7 +180,7 @@ echo -en '\033[1mDo you want to remove this script\033[33;1m ("archforme.sh")\03
 while true; do
     read yn
     case $yn in
-        [Yy]* ) 
+        [Yy]* )
             rm -f archforme.sh
             echo -en '\033[32;1mDone.\033[0m\n'
             break;;
@@ -181,7 +197,3 @@ echo -en '\033[32;1m***                  Good luck! :-)                 ***\033[
 echo -en '\033[32;1m*******************************************************\033[0m\n'
 echo -en ''
 echo -en ''
-
-# zsh
-source .zshrc
-
